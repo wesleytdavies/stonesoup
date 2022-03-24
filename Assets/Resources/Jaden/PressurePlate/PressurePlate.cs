1b9ae85537184f7c6272e6e4a7cb56f7714979d2
jaden <cep450@nyu.edu>
2022-03-24 14:52:18 -0400

    pressure plate now displays

--- Assets/Resources/Jaden/PressurePlate/PressurePlate.cs
+++ Assets/Resources/Jaden/PressurePlate/PressurePlate.cs
@@ -8,6 +8,10 @@ public class PressurePlate : TripMine
     //so can be used as puzzle elements, unlike trip mines 
     [SerializeField] Sprite pressedSprite;
 
+    void Update() {
+        renderer.enabled = true;
+    }
+
     //pressure plates can be tripped by enemies as well as players 
     //can be tripped by anything honestly.
     public override void OnTriggerEnter2D(Collider2D collision) {
